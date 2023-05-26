12246d982656658aac169e9a505e0fbc6bcbd801
Nutulous <127353946+Nutulous@users.noreply.github.com>
2023-05-25 22:21:04 -0500

    Funky Files
    This includes a new Funky page that continues to test and demo the flexbox and flex directions. Goal tomorrow or Saturday is to get the flexboxes to rotate in a cascading pattern. Hoping there's an area of FlexBox to allow for such. Also doing an additional pr for the relative changes to the other files to account for the new Funky page.

---
+++ Funky.js
@@ -0,0 +1,50 @@
+const funkyRotate = () => {
+    const flexBox = document.getElementById("FunkyArea");
+    const style = window.getComputedStyle(flexBox)
+    const currentDirection = style.getPropertyValue("flex-direction");
+    
+    if (currentDirection === "row")
+        flexBox.style.flexDirection = "column"
+    else if (currentDirection === "column")
+        flexBox.style.flexDirection = "row"
+}
+const funkyRotate2 = () => {
+    const flexBox = document.getElementById("FunkyArea2");
+    const style = window.getComputedStyle(flexBox)
+    const currentDirection = style.getPropertyValue("flex-direction");
+    
+    if (currentDirection === "row")
+        flexBox.style.flexDirection = "column"
+    else if (currentDirection === "column")
+        flexBox.style.flexDirection = "row"
+}
+const funkyRotate3 = () => {
+    const flexBox = document.getElementById("FunkyArea3");
+    const style = window.getComputedStyle(flexBox)
+    const currentDirection = style.getPropertyValue("flex-direction");
+    
+    if (currentDirection === "row")
+        flexBox.style.flexDirection = "column"
+    else if (currentDirection === "column")
+        flexBox.style.flexDirection = "row"
+}
+const funkyRotate4 = () => {
+    const flexBox = document.getElementById("FunkyArea4");
+    const style = window.getComputedStyle(flexBox)
+    const currentDirection = style.getPropertyValue("flex-direction");
+    
+    if (currentDirection === "row")
+        flexBox.style.flexDirection = "column"
+    else if (currentDirection === "column")
+        flexBox.style.flexDirection = "row"
+}
+const funkyRotate5 = () => {
+    const flexBox = document.getElementById("FunkyArea5");
+    const style = window.getComputedStyle(flexBox)
+    const currentDirection = style.getPropertyValue("flex-direction");
+    
+    if (currentDirection === "row")
+        flexBox.style.flexDirection = "column"
+    else if (currentDirection === "column")
+        flexBox.style.flexDirection = "row"
+}
