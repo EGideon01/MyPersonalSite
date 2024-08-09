3512440435885591a5059f42cc11be1786f61158
EspenGideon <127353946+EGideon01@users.noreply.github.com>
2024-08-08 23:24:31 -0500

    Update Espen.js

--- Espen.js
+++ Espen.js
@@ -1,46 +1,49 @@
-console.log("Wassup G")
+/*Attempting to utilize block coding. 
 
-//var list
-let bigolNum = 5
-let smolNum = 1
+With block coding, we have the format:
+    1 - Define/Declare
+         In this step, we are simply stating the constant or initial value (for variables) that will affect or be affected by our action code.
+    2 - Action
+        As implied, this step will dictate what happens to our site compenents based upon user input.
+    3 - Output
+        This demonstrates the result of the reaction between both our code and user input. This is difficult to visualize without viewing the holistic makeup of the code. Viewing the HTML & CSS code can help better understand the impact JS has on the site.
+
+
+*/
+
+// 1 - Define/Declare
+let bigNum = 5 //var list
+let smallNum = 1
 let averageNum = 3
-//valid vars, in case they're needed
-    //let _example = #
-    //let $example = #
-        //note: this still makes them a different var than just "example"
-
-//const list
-const gMan = 25
-const aMan = "Woah, das a big number"
+const aMan = 25 //const list
+const bMan = "Big Number"
 const number1 = 10e4
 
-//symbols list (new to js)
-const ayo = Symbol("ayooo")
-let sampleObjectKey = Symbol("disaSampleDawg")
-let regularSampleDawg = {
-    name: "Dawg",
 
-    //adding symbol as a key
-    [sampleObjectKey]: "One, two, three and to the fo'" //brackets for object keys
-}
-
-//For Loops
+// 2 - Action
+    //For Loops
 for (let i = 0; i < 5; i++) {
     text = "Dis da number:"  + i + "<br>"
   }
 
-//Button Inclusion
+    //Button Inclusion
 function showMic() {
     var img = document.getElementById('mic');
     img.style.visibility = 'visible';
 }
-//Button Inclusion
 function hideMic() {
     var img = document.getElementById('mic');
     img.style.visibility = 'hidden';
 }
 
 
-console.log(((bigolNum*smolNum*averageNum/gMan)*number1) + " " + "⬅" + " " + aMan);
-console.log(ayo.description)
-console.log(regularSampleDawg)
+// 3 - Output
+console.log("Hello There")
+console.log(((bigNum*smallNum*averageNum/aMan)*number1) + " " + "⬅" + " " + bMan);
+
+
+
+//valid vars, in case they're needed
+    //let _example = #
+    //let $example = #
+        //note: this still makes them a different var than just "example"
